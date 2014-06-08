72ffd916fa76fb7e91c0ac8a7f1b57d0b3de0242
Frank <frankiecast@gmail.com>
2014-06-07 21:51:02 -0400

    process refactor

--- src/tributary/bpmn.clj
+++ src/tributary/bpmn.clj
@@ -20,7 +20,11 @@
 (defn- dtype
   [node]
   (let [_x (clojure.string/split node #":")]
-    (assoc {} :lang (_x 0) :ref (_x 1))))
+    (condp = (count _x)
+      1 (assoc {} :lang nil :ref (_x 1))
+      2 (assoc {} :lang (_x 0) :ref (_x 1))
+      3 (assoc {} :lang (_x 1) :ref (_x 2))
+     )))
 
 ;; Data setup
 
@@ -29,17 +33,21 @@
 (defn- data-name
   "Takes a data id and pulls name and collection indicator"
   [data-id root namekw]
-  (set/rename-keys
-   (select-keys
-    (:attrs (zip/node (zx/xml1-> root (pf namekw) (zx/attr= (first data-attrs) data-id))))
-    data-attrs) {(first data-attrs) :id}))
-
+  (update-in
+   (set/rename-keys
+    (conj {:isCollection "false"}
+         (select-keys
+          (:attrs
+           (zip/node
+            (zx/xml1-> root (pf namekw) (zx/attr= (first data-attrs) data-id))))
+          data-attrs))
+    {(first data-attrs) :id}) [:isCollection] symbol))
 
 (defn- data-iospec
   "root can be process or task. Takes the result of :ioSpecifiction 'iospec' zip node
   then uses the :itemSubjectRef as :id filter for completing the definition"
-  [root namekw]
-  (let [_di (zx/xml-> root  (pf :ioSpecification) (pf :dataInput))]
+  [specroot root namekw]
+  (let [_di (zx/xml-> specroot  (pf :ioSpecification) (pf :dataInput))]
     (mapv #(data-name ((first data-attrs) (:attrs (zip/node %))) root namekw) _di)))
 
 (defn- data-type
@@ -50,14 +58,20 @@
 
 (defn- global-data
   "Pulls global process definitions"
-  []
+  [pnode]
   (mapv #(data-type %  *zip* :itemDefinition)
-        (data-iospec (zx/xml1-> *zip* (pf :process)) :dataObject)))
+        (data-iospec pnode pnode :dataObject)))
 
-(defn- data-context
-  "Develop the data reference context details"
-  []
-  (assoc {} :process-data (global-data)))
+(defn- task-data
+  "Task data may be global or locally defined.
+  If locally defined, it does not have type information"
+  [tnode proot]
+  (let [_ids (mapv #(assoc {} :id (:itemSubjectRef (:attrs (zip/node %))))
+              (zx/xml-> tnode (pf :ioSpecification) (pf :dataInput)))
+        _dts (mapv #(data-type  % *zip* :itemDefinition) _ids)
+        ;_dts (mapv #(assoc {} :id %) _ids)
+        ]
+    _dts))
 
 ;; Process setup
 ; Each lane in the laneset represents a flow
@@ -101,29 +115,59 @@
                                   (pf :sequenceFlow)
                                   (zx/attr= :sourceRef (:id _s))) _o))))
 
-(defn- lane-defs
-  "Builds the individual lanes"
-  [ls-items]
-  (for [ln ls-items
-        :let [_n0 (zip/node ln)
-              _ld  (lane-seq _n0 (select-keys (:attrs _n0) [:id :name]))]]
-     _ld))
+(defn- finish-node
+  [node node-type proot]
+  (condp = node-type
+    :startEvent       nil
+    :task             (assoc {} :data (task-data node proot))
+    :userTask         (assoc {} :data (task-data node proot))
+    :exclusiveGateway nil
+    :endEvent         nil
+    ))
 
+(defn- node-def
+  [node-id proot _cat]
+  (let [_t (first (filter #(= (:id %) node-id) _cat))
+        _n (zx/xml1-> proot (:type _t) (zx/attr= :id node-id))
+        _a (conj _t (:attrs (zip/node _n))
+                 {:type (keyword (:ref (dtype (str (:type _t)))))})
+        ]
+    (conj _a (finish-node _n (:type _a) proot))))
+
+(defn- flow-def
+  [flowref proot]
+  (let [_fr (:attrs (zip/node flowref))
+        _ct (map #(assoc {} :id (:id (:attrs %)) :type (:tag %)) (zip/children proot))
+        _fn (map zx/text (zx/xml-> flowref (pf :flowNodeRef)))
+        _fd (assoc-in _fr [:nodes] (mapv #(node-def % proot _ct) _fn))
+        ]
+    _fd)
+  )
+
+(defn- flowset-def
+  [flow-set proot]
+  (let [_fl (mapv #(flow-def % proot) (zx/xml-> flow-set (pf :lane)))]
+    _fl))
+
+(defn- process-def
+  [proot]
+  (let [_pn (assoc-in (:attrs (zip/node proot)) [:process-data]
+                      (global-data proot))
+        _pn (assoc-in _pn [:flowsets] (mapv #(flowset-def % proot)
+                                           (zx/xml-> proot (pf :laneSet))))]
+    _pn))
 
 (defn- process-context
-  "Builds out the processes defined in lanesets"
   []
-  (assoc {} :processes
-    (into [] (lane-defs (zx/xml-> *zip* (pf :process) (pf :laneSet) (pf :lane))))))
-
+  (assoc {} :processes (mapv #(process-def %) (zx/xml-> *zip* (pf :process)))))
 
 (defn context
   [parse-block]
   (binding [*zip* (:zip parse-block) *prefix* (:ns parse-block)]
-    (conj (data-context) (process-context))))
+    (process-context)))
 
 (use 'clojure.pprint)
 
 (def _s0 (tu/parse-source "resources/Valid Ticket-LD.bpmn"))
 (def _t0 (context _s0))
-(pprint (:process-data _t0))
+(pprint _t0)
